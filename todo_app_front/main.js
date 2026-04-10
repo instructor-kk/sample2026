016ac680e69c636202494d0f20dca56ed7477d03
instructor-kk <kuroko.keiko@gmail.com>
2026-04-10 10:35:35 +0900

    Lesson13追加

--- todo_app_front/main.js
+++ todo_app_front/main.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://127.0.0.1:5000/tasks';
+// const API_URL = 'http://127.0.0.1:5000/tasks';
+const API_URL = 'http://43.207.118.54/api/tasks';
+
 
 // タスク一覧を表示
 async function renderTasks() {
